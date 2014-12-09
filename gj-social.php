ce8d0a10569bb21a4df8ee44cc75aad1078f971d
Patrick Shampine <pshampine@gunnjerkens.com>
2014-12-08 16:20:53 -0800

    Initial files

---
+++ gj-social.php
@@ -0,0 +1,9 @@
+/*
+ * Plugin Name: GJ Social
+ * Plugin URI: https://github.com/GunnJerkens/gj-social
+ * Description: Pull down social media streams to json store for usage throughout a WP theme.
+ * Version: 0.1.0
+ * Author: GunnJerkens
+ * Author URI: http://gunnjerkens.com
+ * License: MIT
+ */
