ed515653d0c2682babceb462576b641fd2f580e4
Alex Medina <alxdna@gmail.com>
2018-04-05 10:55:55 -0700

    updates error message and returns instead of echos

--- gj-social.php
+++ gj-social.php
@@ -222,10 +222,11 @@ class gjSocial {
         '/'.$page.'/feed'.$fields.'&limit='.$count
       );
     } catch(Facebook\Exceptions\FacebookResponseException $e) {
-      echo 'Graph returned an error: ' . $e->getMessage();
+      return 'Graph returned an error: ' . $e->getMessage();
       exit;
     } catch(Facebook\Exceptions\FacebookSDKException $e) {
-      echo 'Facebook SDK returned an error: ' . $e->getMessage();
+      return 'Facebook SDK returned an error: ' . $e->getMessage();
+      exit;
     }
     return $response->getDecodedBody();
   }
@@ -264,10 +265,11 @@ class gjSocial {
         '/'.$biz_account_id.'/media'.$fields.'&limit='.$count
       );
     } catch(Facebook\Exceptions\FacebookResponseException $e) {
-      echo 'Graph returned an error: ' . $e->getMessage();
+      return 'Graph returned an error: ' . $e->getMessage();
       exit;
     } catch(Facebook\Exceptions\FacebookSDKException $e) {
-      echo 'Facebook SDK returned an error: ' . $e->getMessage();
+      return 'Facebook SDK returned an error: ' . $e->getMessage();
+      exit;
     }
 
     return $response->getDecodedBody();
